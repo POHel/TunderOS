b93e51832849571ac40e764d5e580e6885066081
SKATT <igorkokovin597864@gmail.com>
2025-05-13 17:23:47 +0500

    test

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -2,6 +2,5 @@ log = Logger("system")
 log.debug('debug test')
 log.info('test info')
 log.error('test error')
-log.warning('test warn') #huihui
+log.warning('test warn')
 log.critical('crit err test')
-#dncen
