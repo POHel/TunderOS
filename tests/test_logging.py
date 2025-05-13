14a4f96076b3d1eda93d04776caab4e6b4b20e05
SKATT <igorkokovin597864@gmail.com>
2025-05-13 16:48:04 +0500

    check

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -2,5 +2,5 @@ log = Logger("system")
 log.debug('debug test')
 log.info('test info')
 log.error('test error')
-log.warning('test warn')
+log.warning('test warn') #huihui
 log.critical('crit err test')
