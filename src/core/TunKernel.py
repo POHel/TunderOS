27d4c1d0729ace86aa5c300ea367ad963bb7ae7d
SKATT <igorkokovin597864@gmail.com>
2025-06-27 00:50:28 +0500

    начало разработки Kernel

--- src/core/TunKernel.py
+++ src/core/TunKernel.py
@@ -1,1 +1,13 @@
 #Kernel TunderOS
+#created by SKATT
+from pathlib import Path
+import os
+import sys
+INIT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(INIT_DIR)
+from TNFS.TNFS import TNFS
+from core.users import UserManager
+from security.SELinux import SELinux
+from libs.logging import Logger
+from libs.CrashHandler import CrashHandler
+
